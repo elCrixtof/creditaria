2a687c949a192d897e22d8e42583b034546f9df5
Christian Acosta <cristian117acosta@hotmail.com>
2026-02-02 14:00:32 -0600

    feat: se instalo axios para probar la conexion con el backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [msg, setMsg] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/')
+      .then(res => setMsg(res.data.message))
+      .catch(err => console.error(err))
+  }, [])
 
   return (
     <>
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <h1>Frontend con React</h1>
+        <p>Respuesta del servidor: <strong>{msg}</strong></p>
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
